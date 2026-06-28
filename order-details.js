d4b13051d0aeef9e45e50252885e1627bea16ed2
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-06-28 13:13:32 +0300

    Update order-details.js

--- order-details.js
+++ order-details.js
@@ -1,1 +1,48 @@
+import { db } from "./firebase.js";
 
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const params = new URLSearchParams(window.location.search);
+
+const id = params.get("id");
+
+async function loadOrder() {
+
+    if (!id) return;
+
+    const snap = await getDoc(doc(db, "orders", id));
+
+    if (!snap.exists()) {
+        alert("أمر الإنتاج غير موجود");
+        return;
+    }
+
+    const order = snap.data();
+
+    document.getElementById("orderNo").textContent =
+        "رقم المستند: " + id;
+
+    document.getElementById("customer").textContent =
+        order.customer;
+
+    document.getElementById("glassType").textContent =
+        order.glassType;
+
+    document.getElementById("width").textContent =
+        order.width + " مم";
+
+    document.getElementById("height").textContent =
+        order.height + " مم";
+
+    document.getElementById("qty").textContent =
+        order.qty;
+
+    document.getElementById("status").textContent =
+        order.status;
+
+}
+
+loadOrder();
