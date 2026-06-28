5bf48530cf9d74789a9cdb75c12eedf3ecb59bd1
rashedhathalmic-crypto <rashedhathalmic@gmail.com>
2026-06-28 13:18:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,14 +10,16 @@ form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const order = {
-        customer: document.getElementById("customer").value,
-        glassType: document.getElementById("glassType").value,
-        width: Number(document.getElementById("width").value),
-        height: Number(document.getElementById("height").value),
-        qty: Number(document.getElementById("qty").value),
-        date: new Date().toISOString(),
-        status: "جديد"
-    };
+    orderNo: "2026-" + Date.now(),
+    customer: document.getElementById("customer").value,
+    glassType: document.getElementById("glassType").value,
+    width: Number(document.getElementById("width").value),
+    height: Number(document.getElementById("height").value),
+    qty: Number(document.getElementById("qty").value),
+    date: new Date().toISOString(),
+    status: "جديد",
+    stage: "استلام"
+};
 
     try {
         await addDoc(collection(db, "orders"), order);
